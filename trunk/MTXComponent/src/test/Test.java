9813ffa5cafe3eafbbf609bb0741eb13b89eb19f
yin.pingyong@yahoo.com.cn <yin.pingyong@yahoo.com.cn@079736d8-895a-14bb-f388-3d3d15337207>
2012-07-17 05:27:23 +0000

    初始导入。

---
+++ trunk/MTXComponent/src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import com.mt.common.dynamicDataDef.FieldMapSet;
+
+/**
+ * 
+ * @Author: Ryan
+ * 
+ * 2012-7-17
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FieldMapSet fms = new FieldMapSet("pages");
+		System.err.println(fms);
+	}
+
+}
