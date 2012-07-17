04bc17a52acbea81cbb198697c761d624f642ed1
yin.pingyong <yin.pingyong@yahoo.com.cn>
2012-07-17 10:15:03 +0000

    初始导入。

---
+++ LocalStore/src/com/mt/common/localStore/TextField.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mt.common.localStore;
+
+/**
+ * 可支持简洁文本输出的类型需实现这个接口
+ * Created by NetBeans.
+ *
+ * @Author: hanhui
+ * @Date: 2010-7-17
+ */
+public interface TextField {
+
+    public void fromString(String v);
+}
