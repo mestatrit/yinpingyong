05c5e435969c42684c93c25ecee05aa23ad5a26b
yin.pingyong <yin.pingyong@yahoo.com.cn>
2012-10-23 08:31:54 +0000

    获取下拉框值

--- mtxComponent/src/test/combobox/MTXAutoCompletionComboBoxTester.java
+++ mtxComponent/src/test/combobox/MTXAutoCompletionComboBoxTester.java
@@ -2,10 +2,15 @@ package test.combobox;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -22,9 +27,12 @@ public class MTXAutoCompletionComboBoxTester {
 
 	private JFrame frame;
 	private JPanel searchPanel;
+	private JComboBox pBox;
 	private MTXAutoCompletionComboBox box;
 	private MTXAutoCompletionComboPanel boxPanel;
 	private MTXAutoCompletionDoubleComboPanel doubleBoxPanel;
+	
+	private JButton btn1,btn2,btn3,btn4;
 	
 	private Object[] testV = { 
 			new NameCodeItem("华为23434", "dfdsf1"),
@@ -41,6 +49,7 @@ public class MTXAutoCompletionComboBoxTester {
 
 	public MTXAutoCompletionComboBoxTester() {
 		this.initComponent();
+		this.initAction();
 	}
 
 	private void initComponent() {
@@ -55,6 +64,9 @@ public class MTXAutoCompletionComboBoxTester {
 	}
 
 	private void initSearchPanel() {
+		this.pBox = new JComboBox(testV);
+		this.pBox.setSelectedIndex(0);
+		
 		this.box = new MTXAutoCompletionComboBox(testV);
 		this.box.setAutoCSelectedIndex(1);
 		
@@ -67,15 +79,72 @@ public class MTXAutoCompletionComboBoxTester {
 		
 		this.searchPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		this.searchPanel.add(new JLabel("普通下拉框："));
-		this.searchPanel.add(new JComboBox(testV));
+		this.searchPanel.add(pBox);
 		this.searchPanel.add(new JLabel("搜索下拉框："));
 		this.searchPanel.add(box);
 		this.searchPanel.add(new JLabel("搜索下拉框面板："));
 		this.searchPanel.add(boxPanel);
 		this.searchPanel.add(new JLabel("搜索下拉框双组合面板："));
 		this.searchPanel.add(doubleBoxPanel);
+		
+		this.btn1 = new JButton("普通");
+		this.btn2 = new JButton("搜索");
+		this.btn3 = new JButton("搜面板");
+		this.btn4 = new JButton("双搜面板");
+		this.searchPanel.add(btn1);
+		this.searchPanel.add(btn2);
+		this.searchPanel.add(btn3);
+		this.searchPanel.add(btn4);
 	}
-
+	
+	private void initAction() {
+		this.btn1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				NameCodeItem item = (NameCodeItem)pBox.getSelectedItem();
+				JOptionPane.showConfirmDialog(frame, item.getName());
+			}
+		});
+		
+		this.btn2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				/**
+				 * 获取选中项
+				 */
+				Object obj = box.getAutoCSelectedItem();
+				if (obj != null) {
+					JOptionPane.showConfirmDialog(frame, ((NameCodeItem)obj).getName());	
+				}
+				
+				/**
+				 * 获取当前输入框值
+				 */
+				JOptionPane.showConfirmDialog(frame, box.getEditData());	
+			}
+		});
+		
+		this.btn3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boxPanel.getListItem();
+				boxPanel.getCurFListItem();
+			}
+		});
+		
+		this.btn4.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+	}
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 
